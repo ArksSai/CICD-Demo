fa5ccfd5f4fac118e689e2b17cf5e152bb32fc79
ArksSai <arksone1@gmail.com>
2021-08-28 23:22:46 +0800

    update terraform

--- Terraform/application-server/user_data.sh
+++ Terraform/application-server/user_data.sh
@@ -1,2 +1,17 @@
 #! /bin/bash
-echo "Application server"
+
+sudo yum update -y
+
+sudo amazon-linux-extras install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+cat << EOT > start-website
+/bin/sh -e -c 'echo \$(aws ecr get-login-password --region ap-northeast-1) | docker login -u AWS --password-stdin ${repository_url}'
+sudo docker pull ${repository_url}:release
+sudo docker run -p 80:8000 ${repository_url}:release
+EOT
+
+sudo mv start-website /var/lib/cloud/scripts/per-boot/start-website
+sudo chmod +x /var/lib/cloud/scripts/per-boot/start-website
+/var/lib/cloud/scripts/per-boot/start-website
